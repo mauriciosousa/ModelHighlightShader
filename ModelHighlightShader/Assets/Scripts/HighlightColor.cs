2c3a4b607d970f163ad5c7f21855b8517b39b4a9
Mauricio Sousa <antonio.sousa@ist.utl.pt>
2020-06-24 19:37:49 +0100

    init
    model, target, shader, material, and script to update material

---
+++ ModelHighlightShader/Assets/Scripts/HighlightColor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightColor : MonoBehaviour
+{
+    public Transform target;
+    public float radius = 1f;
+
+    private MeshRenderer _meshRenderer;
+    private Material _material;
+
+    void Start()
+    {
+        _meshRenderer = GetComponent<MeshRenderer>();
+        _material = Resources.Load("Materials/Highlight") as Material;
+        _meshRenderer.material = _material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _meshRenderer.material.SetVector("_TargetPosition", transform.position);
+        _meshRenderer.material.SetFloat("_Radius", radius);
+    }
+}
